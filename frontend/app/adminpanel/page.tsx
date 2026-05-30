e29be9c13f3838d033f4c34e55b1483ebccdff34
maotek <maoshengj@hotmail.com>
2026-05-30 20:43:02 +0200

    better identifier

--- frontend/app/adminpanel/page.tsx
+++ frontend/app/adminpanel/page.tsx
@@ -1,7 +1,8 @@
+import { hostname } from "node:os";
 import { connection } from "next/server";
 import { AdminApp } from "../components/LiveQuiz";
 
 export default async function AdminPanelPage() {
   await connection();
-  return <AdminApp frontendPod={process.env.POD_NAME || process.env.HOSTNAME || "local"} />;
+  return <AdminApp frontendPod={process.env.POD_NAME || hostname()} />;
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,8 @@
+import { hostname } from "node:os";
 import { connection } from "next/server";
 import { PlayerApp } from "./components/LiveQuiz";
 
 export default async function Home() {
   await connection();
-  return <PlayerApp frontendPod={process.env.POD_NAME || process.env.HOSTNAME || "local"} />;
+  return <PlayerApp frontendPod={process.env.POD_NAME || hostname()} />;
 }
